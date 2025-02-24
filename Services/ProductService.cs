199937ab62d30783f84c0670028d6bd00506961b
Prince Bhagwat <104710793+princebhagwat24@users.noreply.github.com>
2025-02-24 13:46:52 +0530

    Add files via upload

---
+++ Services/ProductService.cs
@@ -0,0 +1,40 @@
+﻿using CRUDWithRepository.Models;
+using CRUDWithRepository.Repositories.Interfaces;
+
+namespace CRUDWithRepository.Services
+{
+    public class ProductService
+    {
+        private readonly IGenericRepository<Product> _productRepository;
+
+        public ProductService(IGenericRepository<Product> productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<IEnumerable<Product>> GetAllProductsAsync()
+        {
+            return await _productRepository.GetAllAsync();
+        }
+
+        public async Task<Product> GetProductByIdAsync(int id)
+        {
+            return await _productRepository.GetByIdAsync(id);
+        }
+
+        public async Task AddProductAsync(Product product)
+        {
+            await _productRepository.AddAsync(product);
+        }
+
+        public async Task UpdateProductAsync(Product product)
+        {
+            await _productRepository.UpdateAsync(product);
+        }
+
+        public async Task DeleteProductAsync(int id)
+        {
+            await _productRepository.DeleteAsync(id);
+        }
+    }
+}
