6f3d4ad06cfda525503b98922f2dee3b03292cb4
Prince Bhagwat <104710793+princebhagwat24@users.noreply.github.com>
2025-02-24 13:45:40 +0530

    Add files via upload

---
+++ Repositories/Implementations/ProductRepository.cs
@@ -0,0 +1,10 @@
+﻿using CRUDWithRepository.Data;
+using CRUDWithRepository.Models;
+
+namespace CRUDWithRepository.Repositories.Implementations
+{
+    public class ProductRepository : GenericRepository<Product>
+    {
+        public ProductRepository(ApplicationDbContext context) : base(context) { }
+    }
+}
